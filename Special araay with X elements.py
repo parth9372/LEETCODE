faab306c18cee356d2957e81fe7007820cf52708
parth9372 <152137074+parth9372@users.noreply.github.com>
2024-05-28 19:31:12 +0530

    LeetCode problem solving with python

---
+++ Special araay with X elements.py
@@ -0,0 +1,33 @@
+#Input: nums = [3,5]
+#Output: 2
+#Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
+
+class Solution:
+    def specialArray(self, nums):
+        nums.sort(reverse=True)
+
+        if nums[-1] > len(nums):
+            return len(nums)
+
+        if nums[0] == 0:
+            return -1
+
+        l = 0
+        r = len(nums) - 1
+        m = 0
+
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] > m:
+                l = m + 1
+            elif nums[m] < m:
+                r = m - 1
+            else:
+                return -1
+
+        return m + 1 if nums[m] > m else m
+
+#Example
+nums = [2,3]
+solution = Solution()
+print(solution.specialArray(nums))
