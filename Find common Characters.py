2cb42c3fa6eb55d804f87c65ae9cec7af6d0b6b2
parth9372 <152137074+parth9372@users.noreply.github.com>
2024-06-05 10:31:02 +0530

    LeetCode problem solving with python

---
+++ Find common Characters.py
@@ -0,0 +1,24 @@
+#Input: words = ["bella","label","roller"]
+#Output: ["e","l","l"]
+
+class Solution:
+    def commonChars(self, words):
+        min_freq = [float('inf')] * 26
+
+        for word in words:
+            char_count = [0] * 26
+            for char in word:
+                char_count[ord(char) - ord('a')] += 1
+            for i in range(26):
+                min_freq[i] = min(min_freq[i], char_count[i])
+
+        result = []
+        for i in range(26):
+            result.extend([chr(i + ord('a'))] * min_freq[i])
+
+        return result
+
+#Example
+words = ["bella","label","roller"]
+solution = Solution()
+print(solution.commonChars(words))
